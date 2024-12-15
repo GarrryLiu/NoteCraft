11de18c5818eeca7bf29d7a12e58c9ae2bc91b95
Zinc Zhao <zinc.zhao@emory.edu>
2024-12-15 01:33:14 -0500

    merge changes from gary's branch

--- get_result.py
+++ get_result.py
@@ -10,16 +10,28 @@ img = cv2.imread(image_path)
 # Split the image into parts
 split_symbols = extract_symbol(img)
 
-predict_result = []
-# Predict for each split image
-for i, row in enumerate(split_symbols):
-    predict_result_row = []
-    for j, symbol in enumerate(row):
-        cv2.imwrite(f"./outputs/split_notes/processed_{i}_{j}.png", symbol)
-        model = load_model("./model/NN_model.pkl")
-        prediction = predict(symbol, model)
-        predict_result_row.append(prediction)
-    predict_result.append(predict_result_row)
+def get_actl_prediction(prediction):
+    identifiers = ['4', '8', '16', '32', '#', 'bar', 'clef', 'flat', 'natural', 'chord', 'dot', 'p']
+    for i in identifiers:
+        if i in prediction:
+            return i
+
+def get_note_types(split_symbols):
+    predict_result = []
+    # Predict for each split image
+    for i, row in enumerate(split_symbols):
+        predict_result_row = []
+        for j, symbol in enumerate(row):
+            cv2.imwrite(f"./outputs/split_notes/processed_{i}_{j}.png", symbol)
+            model = load_model("./model/NN_model.pkl")
+            prediction = predict(symbol, model)
+            prediction = get_actl_prediction(prediction)
+            predict_result_row.append(prediction)
+        predict_result.append(predict_result_row)
+    return predict_result
+
+predict_result = get_note_types(split_symbols)
+    
 
 print("Predictions:")
 for i, row in enumerate(predict_result):
